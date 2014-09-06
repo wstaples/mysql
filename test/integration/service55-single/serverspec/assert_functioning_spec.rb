aa16b0044f49c4e143b74cae4391e909600b657e
Sean OMeara <someara@opscode.com>
2014-09-06 16:14:09 -0400

    [ci skip] savegame: adding serverspec for server55-single and server55-multi

---
+++ test/integration/service55-single/serverspec/assert_functioning_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+
+cmd = "/usr/bin/mysql"
+cmd << " -h 127.0.0.1"
+cmd << " -P 3306"
+cmd << " -u root"
+cmd << " -pilikerandompasswords"
+cmd << " -e \"SELECT Host,User,Password FROM mysql.user WHERE User='root' OR User='repl'; \""
+
+describe command(cmd) do
+  it { should return_exit_status 0 }
+end
+
