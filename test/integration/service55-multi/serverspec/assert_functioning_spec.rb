085de8035e5fb1dedfc3fb4aaf8e51cbae728fe5
Sean OMeara <someara@opscode.com>
2014-09-06 16:21:27 -0400

    [ci skip] fixing cops

--- test/integration/service55-multi/serverspec/assert_functioning_spec.rb
+++ test/integration/service55-multi/serverspec/assert_functioning_spec.rb
@@ -2,22 +2,22 @@ require 'serverspec'
 
 include Serverspec::Helper::Exec
 
-instance_1_cmd = "/usr/bin/mysql"
-instance_1_cmd << " -h 127.0.0.1"
-instance_1_cmd << " -P 3307"
-instance_1_cmd << " -u root"
-instance_1_cmd << " -pnever\\ gonna\\ give\\ you\\ up"
+instance_1_cmd = '/usr/bin/mysql'
+instance_1_cmd << ' -h 127.0.0.1'
+instance_1_cmd << ' -P 3307'
+instance_1_cmd << ' -u root'
+instance_1_cmd << ' -pnever\\ gonna\\ give\\ you\\ up'
 instance_1_cmd << " -e \"SELECT Host,User,Password FROM mysql.user WHERE User='root' OR User='repl'; \""
 
 describe command(instance_1_cmd) do
   it { should return_exit_status 0 }
 end
 
-instance_2_cmd = "/usr/bin/mysql"
-instance_2_cmd << " -h 127.0.0.1"
-instance_2_cmd << " -P 3308"
-instance_2_cmd << " -u root"
-instance_2_cmd << " -pnever\\ gonna\\ make\\ you\\ cry"
+instance_2_cmd = '/usr/bin/mysql'
+instance_2_cmd << ' -h 127.0.0.1'
+instance_2_cmd << ' -P 3308'
+instance_2_cmd << ' -u root'
+instance_2_cmd << ' -pnever\\ gonna\\ make\\ you\\ cry'
 instance_2_cmd << " -e \"SELECT Host,User,Password FROM mysql.user WHERE User='root' OR User='repl'; \""
 
 describe command(instance_2_cmd) do
