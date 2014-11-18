7cc8e30d3832d6e13742908032f2e3e00b820f33
Sean OMeara <someara@opscode.com>
2014-11-18 10:21:15 +0100

    [ci skip] - adding redhat to provider mappings

--- libraries/z_provider_mapping.rb
+++ libraries/z_provider_mapping.rb
@@ -4,15 +4,19 @@
 # client
 Chef::Platform.set :platform => :debian, :resource => :mysql_client, :provider => Chef::Provider::MysqlClient::Debian
 Chef::Platform.set :platform => :centos, :resource => :mysql_client, :provider => Chef::Provider::MysqlClient::Rhel
+Chef::Platform.set :platform => :redhat, :resource => :mysql_client, :provider => Chef::Provider::MysqlClient::Rhel
+Chef::Platform.set :platform => :amazon, :resource => :mysql_client, :provider => Chef::Provider::MysqlClient::Rhel
 
 # service
 Chef::Platform.set :platform => :debian, :resource => :mysql_service, :provider => Chef::Provider::MysqlService::Debian
 Chef::Platform.set :platform => :centos, :resource => :mysql_service, :provider => Chef::Provider::MysqlService::Rhel
+Chef::Platform.set :platform => :redhat, :resource => :mysql_service, :provider => Chef::Provider::MysqlService::Rhel
 Chef::Platform.set :platform => :amazon, :resource => :mysql_service, :provider => Chef::Provider::MysqlService::Rhel
 Chef::Platform.set :platform => :omnios, :resource => :mysql_service, :provider => Chef::Provider::MysqlService::Omnios
 
 # config
 Chef::Platform.set :platform => :debian, :resource => :mysql_config, :provider => Chef::Provider::MysqlConfig::Debian
 Chef::Platform.set :platform => :centos, :resource => :mysql_config, :provider => Chef::Provider::MysqlConfig::Rhel
+Chef::Platform.set :platform => :redhat, :resource => :mysql_config, :provider => Chef::Provider::MysqlConfig::Rhel
 Chef::Platform.set :platform => :amazon, :resource => :mysql_config, :provider => Chef::Provider::MysqlConfig::Rhel
 Chef::Platform.set :platform => :omnios, :resource => :mysql_config, :provider => Chef::Provider::MysqlConfig::Omnios
